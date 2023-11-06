1d7fb3e5ac96d0165ef8b0d4c713198fd71a9455
Olli-Pekka Aro <69357695+oppearo@users.noreply.github.com>
2023-11-06 13:43:34 +0200

    3.1-3.4

---
+++ index.js
@@ -0,0 +1,71 @@
+const express = require("express");
+const app = express();
+
+let persons = [
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: 2,
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: 3,
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: 4,
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hello World!</h1>");
+});
+
+app.get("/info", (req, res) => {
+  let numberOfPersons = persons.length;
+  let date = new Date().toString();
+  console.log(date);
+  console.log(`numberOfPersons: ${numberOfPersons}`);
+  res.send(
+    `<p>Phonebook has info for ${numberOfPersons} people<br>${date}</p>`
+  );
+});
+
+app.get("/api/persons", (req, res) => {
+  res.json(persons);
+});
+
+app.get("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  console.log(`id of GET: ${id}`);
+  const person = persons.find((person) => person.id === id);
+
+  if (person) {
+    res.json(person);
+  } else {
+    res.status(404).end();
+  }
+});
+
+app.delete("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  console.log(`id to DELETE: ${id}`);
+
+  persons = persons.find((person) => person.id !== id);
+
+  res.status(204).end();
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
