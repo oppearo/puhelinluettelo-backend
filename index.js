8daac3d5d389074ead1e33e60fff0c15661c9634
Olli-Pekka Aro <69357695+oppearo@users.noreply.github.com>
2023-11-14 14:22:49 +0200

    3.11.

--- index.js
+++ index.js
@@ -33,6 +33,7 @@ let persons = [
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static("dist"));
 app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body")
 );
