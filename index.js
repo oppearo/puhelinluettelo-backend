3d17c597ed951c40a78274ee98f19ba511707177
Olli-Pekka Aro <69357695+oppearo@users.noreply.github.com>
2023-12-04 12:24:18 +0200

    3.13.-3.14.

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
-
 const app = express();
+
+const Person = require("./models/person");
 
 morgan.token("body", (req) => {
   return JSON.stringify(req.body);
@@ -53,24 +55,22 @@ app.get("/info", (req, res) => {
 });
 
 app.get("/api/persons", (req, res) => {
-  res.json(persons);
+  Person.find({}).then((persons) => {
+    res.json(persons);
+  });
 });
 
 app.get("/api/persons/:id", (req, res) => {
   const id = Number(req.params.id);
   console.log(`id of GET: ${id}`);
-  const person = persons.find((person) => person.id === id);
-
-  if (person) {
+  Person.findById(req.params.id).then((person) => {
     res.json(person);
-  } else {
-    res.status(404).end();
-  }
+  });
 });
 
 app.delete("/api/persons/:id", (req, res) => {
   const id = Number(req.params.id);
-  console.log(`id to DELETE: ${id}`);
+  console.log(`id to DELETE: ${req.params.id}`);
 
   persons = persons.find((person) => person.id !== id);
 
@@ -95,20 +95,20 @@ app.post("/api/persons", (req, res) => {
     return res.status(400).json({ error: "name must be unique" });
   }
 
-  const person = {
+  const person = new Person({
     id: getRandomId(),
     name: body.name,
     number: body.number,
-  };
+  });
 
   console.log(JSON.stringify(person));
 
-  persons = persons.concat(person);
-
-  res.json(person);
+  person.save().then((savedPerson) => {
+    res.json(savedPerson);
+  });
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
