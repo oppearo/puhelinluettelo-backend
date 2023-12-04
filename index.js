fd2e638b4bcd79946528ef311dba6250b3b96541
Olli-Pekka Aro <69357695+oppearo@users.noreply.github.com>
2023-12-04 14:26:24 +0200

    add missing error case

--- index.js
+++ index.js
@@ -15,6 +15,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === "CastError") {
     return response.status(400).send({ error: "malformatted id" });
+  } else if (error.name === "ValidationError") {
+    return response.status(400).json({ error: error.message })
   }
 
   next(error);
@@ -31,7 +33,7 @@ app.get("/", (req, res) => {
   res.send("<h1>Hello World!</h1>");
 });
 
-app.post("/api/persons", (req, res) => {
+app.post("/api/persons", (req, res, next) => {
   const body = req.body;
 
   if (!body.name) {
@@ -47,9 +49,12 @@ app.post("/api/persons", (req, res) => {
 
   console.log(JSON.stringify(person));
 
-  person.save().then((savedPerson) => {
-    res.json(savedPerson);
-  });
+  person
+    .save()
+    .then((savedPerson) => {
+      res.json(savedPerson);
+    })
+    .catch((error) => next(error));
 });
 
 const unknownEndpoint = (request, response) => {
