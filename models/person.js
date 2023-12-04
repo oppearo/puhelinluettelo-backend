8aa6b04d0d7fd18b9b2c0c5019531f47acc4d83a
Olli-Pekka Aro <69357695+oppearo@users.noreply.github.com>
2023-12-04 14:54:28 +0200

    3.19.

--- models/person.js
+++ models/person.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const uniqueValidator = require("mongoose-unique-validator");
 
 mongoose.set("strictQuery", false);
 
@@ -16,8 +17,16 @@ mongoose
   });
 
 const personSchema = new mongoose.Schema({
-  name: String,
-  number: String,
+  name: {
+    type: String,
+    minlength: 3,
+    required: true,
+    unique: true,
+  },
+  number: {
+    type: String,
+    required: true,
+  },
 });
 
 personSchema.set("toJSON", {
